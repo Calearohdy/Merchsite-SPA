105a49868d8e3b01ae93351ff3b2e044229320c8
Cale Rohdy <calerohdy@Cales-MacBook-Pro.local>
2019-01-30 15:15:01 -0500

    clean up context api

--- src/context.js
+++ src/context.js
@@ -1,17 +1,34 @@
 import React, { Component } from 'react'
+import {storeProducts, detailProduct} from './data';
 
 const ProductContext = React.createContext();
 // Provider
 // Consumer
 
 class ProductProvider extends Component {
-  render() {
-    return (
-      <ProductContext.Provider value="hello from context">
-        {this.props.children}
-      </ProductContext.Provider>
-    )
-  }
+    state = {
+        products: storeProducts,
+        detailProduct
+    }
+    handleDetail = () => {
+        console.log('hello')
+    }
+
+    addToCart = () => {
+        console.log('from cart')
+    }
+
+    render() { 
+        return (
+        <ProductContext.Provider value={{
+            ...this.state,
+            handleDetail: this.handleDetail,
+            addToCart: this.addToCart
+        }} >
+            {this.props.children}
+        </ProductContext.Provider>
+        )
+    }
 }
 
 const ProductConsumer = ProductContext.Consumer;
