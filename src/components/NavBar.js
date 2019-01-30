11d2752bd618ab4388658a1e8eff2ddb089212c5
Cale Rohdy <calerohdy@Cales-MacBook-Pro.local>
2019-01-30 14:09:51 -0500

    context API added

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import {Link} from 'react-router-dom';
-import logo from '../logo.svg';
+import styled from 'styled-components';
+import {ButtonContainer} from './Button';
 export default class NavBar extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-sm navbar-dark bg-primary px-sm-5">
+      <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
         <Link to='/'>
-        <img src={logo} alt="store" className="navbar-brand" />
+        <div style={{color:'white',fontSize: '30px', float:'left'}}>My Merch</div>
         </Link>
         <ul className="navbar-nav align-items-center">
           <li className="nav-item ml-5">
@@ -16,12 +17,23 @@ export default class NavBar extends Component {
           </li>
         </ul>
         <Link to="/cart" className="ml-auto">
-          <button>
+          <ButtonContainer>
+            <span className="pr-2">
             <i className="fas fa-cart-plus"/>
+            </span>
             My Cart
-          </button>
+          </ButtonContainer>
         </Link>
-      </nav>
+      </NavWrapper>
     )
   }
 }
+
+const NavWrapper = styled.nav`
+background: var(--mainBlue);
+.nav-link {
+  color: var(--mainWhite) !important;
+  font-size: 1.3rem;
+  text-transform: capitalize;
+}
+`
