3391b4b93a2fe4e0703fea2f67ea1912c7148ccc
Cale Rohdy <calerohdy@Cales-MacBook-Pro.local>
2019-02-02 16:21:37 -0500

    add paypal express

---
+++ src/components/Cart/PayPalButton.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import PaypalExpressBtn from 'react-paypal-express-checkout';
+
+export default class PayPalButton extends Component {
+  render() {
+
+    const onSuccess = payment => {
+        this.props.clearCart();
+        this.props.history.push('/')
+    }
+
+    const onCancel = data => {
+
+    }
+
+    const onError = err => {
+
+    }
+
+    let env = "sandbox";
+    let currency = "USD"
+
+    const client = {
+        sandbox: process.env.REACT_APP_PP_ID, // private key - local only
+        production: "your production id"
+    }
+
+        return (
+            <PaypalExpressBtn 
+                env={env}
+                client={client}
+                currency={currency}
+                total={this.props.total}
+                onCancel={onCancel}
+                onError={onError}
+                onSuccess={onSuccess}
+            />
+        )
+  }
+}
